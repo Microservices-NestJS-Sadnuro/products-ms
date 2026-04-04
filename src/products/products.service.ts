a053aa654c29b94c1d63e4ecc5c3d96d7f8dba9e
Sadith Nuñez <sadnr0213@gmail.com>
2026-04-03 23:51:26 -0500

    refactor: implement services folder

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { PrismaService } from './prisma.service';
+import { PrismaService } from './services/prisma.service';
 import { PaginationDto } from 'src/common';
 
 @Injectable()
