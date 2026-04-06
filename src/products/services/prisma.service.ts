402693f29d1d5b15171953d9bdd4a9f723908a54
Sadith Nuñez <sadnr0213@gmail.com>
2026-04-06 10:31:43 -0500

    refactor: migrate database from SQLite to PostgreSQL and update Prisma configuration

--- src/products/services/prisma.service.ts
+++ src/products/services/prisma.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from "@nestjs/common";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
 @Injectable()
 export class PrismaService extends PrismaClient {
     constructor() {
-        const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
-        super({ adapter });
+        const adapter = new PrismaPg({
+            connectionString: process.env.DATABASE_URL,
+        } as any)
+        super({ adapter })
     }
 }
